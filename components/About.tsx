5ca3d8e5cdcddb58fb80c1091b1964d43b360c58
Adeyemo <aa24afl@herts.ac.uk>
2025-03-21 20:54:25 +0000

    updated hero page

--- components/About.tsx
+++ components/About.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { BentoGrid } from './ui/bento-grid'
+// import { gridItems } from '@/data'
 
 const About = () => {
   return (
-    <div className='bg-primary h-100vh'>
-        <BentoGrid/>
+    <div className='bg-teal-700 h-screen'>
+        <h1 className='text-5xl md:text-7xl text-text-primary text-center'>About me</h1>
+        <BentoGrid>
+            {/* {gridItems.map((item) =>)} */}
+        </BentoGrid>
     </div>
   )
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ import { Spotlight } from "./ui/Spotlight";
 function Hero() {
   const [titleNumber, setTitleNumber] = useState(0);
   const titles = useMemo(
-    () => ["amazing", "new", "wonderful", "beautiful", "smart"],
+    () => ["writes code", "designs systems", "thinks a lot", "sleeps"],
     []
   );
 
@@ -30,9 +30,9 @@ function Hero() {
       <div className="container mx-auto">
         <div className="flex gap-8 py-20 lg:py-40 items-center justify-center flex-col">
           <div className="flex gap-4 flex-col text-text-primary">
-            <h1 className="text-5xl md:text-7xl max-w-5xl tracking-tighter text-center font-regular">
-              <span className="text-spektr-cyan-50">This is something</span>
-              <span className="relative flex w-full justify-center overflow-hidden text-center md:pb-4 md:pt-1">
+            <h1 className="text-5xl md:text-7xl mt-20 max-w-5xl tracking-tighter text-center font-regular">
+              <span className="text-spektr-cyan-50">A software engineer that</span>
+              <span className="relative flex w-full justify-center overflow-hidden text-center text-teal-700 md:pb-4 md:pt-1">
                 &nbsp;
                 {titles.map((title, index) => (
                   <motion.span
@@ -58,14 +58,14 @@ function Hero() {
               </span>
             </h1>
 
-            <p className="text-lg md:text-xl leading-relaxed tracking-tight text-muted-foreground max-w-5xl text-center">
-              Managing a small business today is already tough. Avoid further
-              complications by ditching outdated, tedious trade methods. Our
-              goal is to streamline SMB trade, making it easier and faster than
-              ever.
+            <p className="text-lg md:text-xl mt-20 leading-relaxed tracking-tight text-muted-foreground max-w-5xl text-center">
+              This is Ayobami Paul. <br />Currently building projects for fun.
+              It's all about passion, grit, commitment and determination. <br />
+              Nah, that's just to bamboozle. It's actually all about having {" "}
+              <span className="text-teal-700">fun.</span>
             </p>
           </div>
-          <div className="flex flex-row gap-3">
+          <div className="flex flex-row gap-3 mt-10">
             <Button size="lg" className="gap-4" variant="outline">
               Jump on a call <PhoneCall className="w-4 h-4" />
             </Button>
